032628f05560606b6b5bd34c15c78552f43542ad
szymono98 <ostrzolekszymon@gmail.com>
2025-05-05 14:33:01 +0200

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
-/** @type {import('postcss-load-config').Config} */
-export default {
+/**
+ * This type annotation ensures TypeScript can provide type checking and autocompletion
+ * for the PostCSS configuration object, as defined by the `postcss-load-config` package.
+ */
+const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
+
+export default config;
